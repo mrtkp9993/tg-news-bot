02d6ad151af9bb5a7da4fb07f5bfe1cd2187f9f2
Murat Koptur <mkoptur3@gmail.com>
2024-02-11 15:06:57 +0300

    error msg debug

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
+import html
 import json
 import logging
 import os
 import time
+import traceback
 from datetime import datetime
 
 import feedparser
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 fh = logging.FileHandler('bot.log')
@@ -92,12 +95,32 @@ async def send_news(context: ContextTypes.DEFAULT_TYPE) -> None:
         logging.info(f"State saved to file: {state}")
 
 async def error_raise(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logging.info(update.effective_chat.id)
     await context.bot.wrong_method_name()
 
 async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logging.error(f"Update {update} caused error {context.error}")
-    await context.bot.send_message(chat_id=os.environ["DEV_CHAT_ID"], text=context.error)
+    logging.error("Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        "An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    await context.bot.send_message(
+        chat_id=os.environ['DEV_CHAT_ID'], text=message, parse_mode=ParseMode.HTML
+    )
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(os.environ["NEWS_BOT_TOKEN"]).build()
